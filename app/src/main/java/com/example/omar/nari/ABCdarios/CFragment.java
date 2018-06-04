8464b5320c557fb37f7e0982c89a5545ce0367d3
Constantinops <constasophia@gmail.com>
2018-06-04 00:22:20 -0500

    se termino de progrmar los txtview del modulo de abecedarios

--- app/src/main/java/com/example/omar/nari/ABCdarios/CFragment.java
+++ app/src/main/java/com/example/omar/nari/ABCdarios/CFragment.java
@@ -1,6 +1,7 @@
 package com.example.omar.nari.ABCdarios;
 
 import android.content.Context;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,24 +9,48 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.omar.nari.R;
 
 
 public class CFragment extends Fragment {
-
+    TextView letras;
+    ImageView clic;
+    MediaPlayer mp;
     public CFragment() {
         // Required empty public constructor
     }
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_a, container, false);
+
+        View v=inflater.inflate(R.layout.fragment_c, container, false);
+        clic=(ImageView)v.findViewById(R.id.cc);
+        letras=(TextView) v.findViewById(R.id.letra);
+
+        clic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sonido(v);
+
+            }
+        });
+        return v;
     }
 
+
+    public void sonido(View v) {
+
+        mp =MediaPlayer.create(getContext(),R.raw.borrego);
+        mp.start();
+        letras.setText("Aa");
+        //Toast.makeText(getContext(), "hola ", Toast.LENGTH_LONG).show();
+
+    }
 
 
     public interface OnFragmentInteractionListener {
