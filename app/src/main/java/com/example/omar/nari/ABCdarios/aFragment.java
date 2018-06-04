da1c4eeeb078448db749e24974a6da2fe1a47437
Constantinops <constasophia@gmail.com>
2018-06-03 22:57:18 -0500

    se puso un txtview a todos los fragment del modulo de abecedarios

--- app/src/main/java/com/example/omar/nari/ABCdarios/aFragment.java
+++ app/src/main/java/com/example/omar/nari/ABCdarios/aFragment.java
@@ -1,17 +1,23 @@
 package com.example.omar.nari.ABCdarios;
 
 import android.content.Context;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.omar.nari.R;
 
 
 public class aFragment extends Fragment {
+    TextView letras;
+    ImageView clic;
+    MediaPlayer mp;
 
 
     public aFragment() {
@@ -21,10 +27,29 @@ public class aFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
 
         View v=inflater.inflate(R.layout.fragment_a, container, false);
+        clic=(ImageView)v.findViewById(R.id.a);
+        letras=(TextView) v.findViewById(R.id.letra);
+
+        clic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sonido(v);
+
+            }
+        });
         return v;
+    }
+
+
+    public void sonido(View v) {
+
+        mp =MediaPlayer.create(getContext(),R.raw.borrego);
+        mp.start();
+        letras.setText("Aa");
+        //Toast.makeText(getContext(), "hola ", Toast.LENGTH_LONG).show();
+
     }
 
 
@@ -33,3 +58,9 @@ public class aFragment extends Fragment {
         void onFragmentInteraction(Uri uri);
     }
 }
+
+
+
+
+
+
